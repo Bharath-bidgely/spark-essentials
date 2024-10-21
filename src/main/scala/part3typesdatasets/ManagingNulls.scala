530e8a852d745cad42ab53ce1b845e6ec9f77012
bharath-reddy <tinkubharath@gmail.com>
2024-10-21 23:57:11 +0530

    21oct

--- src/main/scala/part3typesdatasets/ManagingNulls.scala
+++ src/main/scala/part3typesdatasets/ManagingNulls.scala
@@ -1,6 +1,7 @@
 package part3typesdatasets
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object ManagingNulls extends App {
   val spark = SparkSession.builder()
@@ -12,10 +13,39 @@ object ManagingNulls extends App {
   spark.sparkContext.setLogLevel("ERROR")
 
   val moviesDF = spark.read.option("inferSchema", "true").json("src/main/resources/data/movies.json")
+  //moviesDF.show(truncate = false)
+  //not null columns
 
-  
+  moviesDF.select(
+    col("Title"),
+    col("Rotten_Tomatoes_Rating"),
+    col("IMDB_Rating"),
+    coalesce(col("Rotten_Tomatoes_Rating"),col("IMDB_Rating")*10)
+  )//.show(truncate = false)
+
+  moviesDF.select(
+    col("Title")
+  ).where(col("Rotten_Tomatoes_Rating").isNull)
+    //.show(2)
+
+  moviesDF.select(
+    col("Title")
+  ).orderBy(col("IMDB_Rating").desc_nulls_last)
+
+  //removing nulls
+  moviesDF.select("Title","IMDB_Rating").na.drop()
+    //.show(2)
+
+  //replace nulls
+  moviesDF.select("Title","IMDB_Rating").na.fill(0,List("IMDB_Rating")).show(2)
+  moviesDF.na.fill (Map(
+    "IMDB_Rating" -> 0,
+    "Rotten_Tomatoes_Rating"   -> 10,
+    "Director" -> "Unknow"
+  )).show(2)
 
 
+  //complex operations
 
 
 }
